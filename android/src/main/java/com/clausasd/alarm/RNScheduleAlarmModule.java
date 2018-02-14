72010981ba24fb1338f2d63dbc11b2944bc2f9d0
Claudiu Florea <fcl@modernweb.ro>
2018-02-14 13:14:59 +0200

    WIP - Android - Basic alarm implementation

--- android/src/main/java/com/clausasd/alarm/RNScheduleAlarmModule.java
+++ android/src/main/java/com/clausasd/alarm/RNScheduleAlarmModule.java
@@ -1,11 +1,14 @@
 
 package com.clausasd.alarm;
 
+import android.app.Notification;
+import android.os.SystemClock;
 import android.widget.Toast;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -15,26 +18,38 @@ import static android.R.attr.type;
 
 public class RNScheduleAlarmModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNScheduleAlarmModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNScheduleAlarmModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @ReactMethod
-  public void schedule(String scheduleAt, String title, String soundUri) {      
-    Long time = Long.parseLong(scheduleAt);
-    AlarmManager alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
-    Intent alarmIntent = new Intent("REACT_NATIVE_ALARM");
-    PendingIntent pendingIntent = PendingIntent.getBroadcast(reactContext, type, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-    alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, pendingIntent);
-    Toast.makeText(reactContext, "Alarm Scheduled", Toast.LENGTH_LONG).show();   
-  }
+    @ReactMethod
+    public void schedule(String scheduleAt, String title, String description, String soundUri) {
+        Long time = Long.parseLong(scheduleAt);
+        Intent notificationIntent = this.createNotificationIntent(title, description, soundUri);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(reactContext, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time, pendingIntent);
+    }
+
+    private Intent createNotificationIntent(String title, String description, String soundUri) {
+        Intent notificationIntent = new Intent(reactContext, RNScheduleAlarmReceiver.class);
+        Notification.Builder builder = new Notification.Builder(reactContext);
+        builder.setContentTitle(title);
+        builder.setContentText(description);
+        builder.setSmallIcon(android.R.drawable.sym_def_app_icon);
+        Notification notification = builder.build();
+        notificationIntent.putExtra("notification-id", 1);
+        notificationIntent.putExtra("fileName", soundUri);
+        notificationIntent.putExtra("notification", notification);
+        return notificationIntent;
+    }
 
 
-  @Override
-  public String getName() {
-    return "RNScheduleAlarm";
-  }
+    @Override
+    public String getName() {
+        return "RNScheduleAlarm";
+    }
 }
--- android/src/main/java/com/clausasd/alarm/RNScheduleAlarmReceiver.java
+++ android/src/main/java/com/clausasd/alarm/RNScheduleAlarmReceiver.java
@@ -1,24 +1,34 @@
 
 package com.clausasd.alarm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
-import android.media.RingtoneManager;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
+
 import android.net.Uri;
 import android.widget.Toast;
+import android.util.Log;
 
-public class RNScheduleAlarmReceiver extends BroadcastReceiver
-{
-    static MediaPlayer player = new MediaPlayer();
+import java.io.IOException;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+public class RNScheduleAlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
-        ringtone.play();
-        Toast.makeText(context, "Alarm Started", Toast.LENGTH_LONG).show();   
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = intent.getParcelableExtra("notification");
+
+        int id = intent.getIntExtra("notification-id", 0);
+        String fileName = intent.getStringExtra("fileName");
+        notification.sound = Uri.parse(String.format("android.resource://%s/%s/%s", context.getPackageName(), "raw", fileName));
+        notificationManager.notify(id, notification);
+
+
     }
 }
